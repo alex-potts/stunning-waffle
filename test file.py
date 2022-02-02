29e2460455b6e41e9e60b7b8d2b5ce0cd480035f
alex-potts <98035938+alex-potts@users.noreply.github.com>
2022-02-02 14:18:37 +0000

    Add files via upload

---
+++ test file.py
@@ -0,0 +1,29 @@
+import urllib.request
+import time
+import os
+
+
+if not os.path.exists('N:\\Documents\\python project'):
+    directory = "python project"
+    parent_dir = "N:/Documents"
+    path = os.path.join(parent_dir, directory)
+    os.mkdir(path)
+
+projectList = {"hangman": 'https://raw.githubusercontent.com/alex-potts/stunning-waffle/main/hangman.py',
+               "cc": 'https://raw.githubusercontent.com/alex-potts/stunning-waffle/main/Ceasar%20cipher.py',
+              }
+
+
+while not os.path.exists('N:\\Documents\\python project\\cc.py'):
+    try:
+        for key, value in projectList.items():
+            path = "N:/Documents/python project/" + key + ".py"
+            while not os.path.exists(path):
+                urllib.request.urlretrieve(value, path)
+                print("Sucess")
+
+    except:
+        print("Failed")
+        time.sleep(2)
+
+exec(open('N:/Documents/python project/hangman.py').read())
---
+++ tets.py
@@ -0,0 +1,6 @@
+import shutil
+
+# absolute path
+src_path = r"N:\Documents\python project\mywords.txt"
+dst_path = r"N:\Documents\mywords.txt"
+shutil.move(src_path, dst_path)

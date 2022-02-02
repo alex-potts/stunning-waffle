5d61c3a60c1b7d1953a5131053a68c770dcd3811
alex-potts <98035938+alex-potts@users.noreply.github.com>
2022-02-02 14:45:04 +0000

    Update mover.py

--- mover.py
+++ mover.py
@@ -1,6 +1,6 @@
 import shutil
 import os 
-if os.path.exists('N:\python project\my words.txt'):
+if os.path.exists('N:\python project\mywords.txt'):
   src_path = r"N:\Documents\python project\mywords.txt"
   dst_path = r"N:\Documents\mywords.txt"
   shutil.move(src_path, dst_path)
